c3c59a97fc40fe94ec56a0e1d4d02bbbc74e9bb6
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-01-07 11:40:24 -0500

    diff --git a/src/bin/event-read-line.rs b/src/bin/event-read-line.rs index bd23625..aa3ff4a 100644 --- a/src/bin/event-read-line.rs +++ b/src/bin/event-read-line.rs @@ -12,7 +12,10 @@ pub fn read_line() -> io::Result<String> {      while let Event::Key(KeyEvent { code, .. }) = event::read()? {          match code {              KeyCode::Enter => { -                break; +                println!("read line:"); +                println!("{:?}", line); +				line = String::from(""); +                //break;              }              KeyCode::Char(c) => {                  line.push(c); @@ -25,6 +28,6 @@ pub fn read_line() -> io::Result<String> {  }
     fn main() {
    -    println!("read line:");
    -    println!("{:?}", read_line());
    +    let result = read_line();
    +	print!("{:?}", result)
     }

---
+++ src/bin/event-read-line.rs
@@ -0,0 +1,33 @@
+//! Demonstrates how to block read characters or a full line.
+//! Just note that crossterm is not required to do this and can be done with `io::stdin()`.
+//!
+//! cargo run --example event-read-char-line
+
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent};
+
+pub fn read_line() -> io::Result<String> {
+    let mut line = String::new();
+    while let Event::Key(KeyEvent { code, .. }) = event::read()? {
+        match code {
+            KeyCode::Enter => {
+                println!("read line:");
+                println!("{:?}", line);
+				line = String::from("");
+                //break;
+            }
+            KeyCode::Char(c) => {
+                line.push(c);
+            }
+            _ => {}
+        }
+    }
+
+    Ok(line)
+}
+
+fn main() {
+    let result = read_line();
+	print!("{:?}", result)
+}
